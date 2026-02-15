b09063fe961c0cfd9ab0383b5c554fd0bb7a0603
mdraushan <mdraushanji22@gmail.com>
2026-02-15 22:08:47 +0530

    PHP-Laravel

---
+++ learn-laravel/resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body>
+    <h1 class="text-center font-bold text-4xl text-green-400">This is my home page</h1>
+</body>
+
+</html>
