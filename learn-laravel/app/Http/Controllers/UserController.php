052476220f9d7cbde9c33e7ba5d0332c3023b39f
mdraushan <mdraushanji22@gmail.com>
2026-02-17 13:44:27 +0530

    PHP-Laravel

--- learn-laravel/app/Http/Controllers/UserController.php
+++ learn-laravel/app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class UserController extends Controller
 {
@@ -23,6 +24,10 @@ class UserController extends Controller
 
     function adminLogin()
     {
-        return view('admin.login');
+        if (View::exists('admin.login')) {
+            return view('admin.login');
+        } else {
+            echo "no view found";
+        }
     }
 }
