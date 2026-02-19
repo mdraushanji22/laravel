99bea952e94ad2c337c93363f099492a8a4941e5
mdraushan <mdraushanji22@gmail.com>
2026-02-19 23:24:31 +0530

    PHP-Laravel

--- learn-laravel/resources/views/home.blade.php
+++ learn-laravel/resources/views/home.blade.php
@@ -12,11 +12,18 @@
 <body>
 
     <h1 class="text-center font-bold text-4xl text-green-400">This is my home page</h1>
-    <h1>{{$username}}</h1>
+    <!-- <h1>{{$username}}</h1>
     <br>
     <h2>
         {{$users[0]}}
-    </h2>
+    </h2> -->
+    @if($username =='Raushan bhai');
+    <h2>This is Raushan bhai</h2>
+    elseif($users=='jilani')
+    <h2>This is jilani</h2>
+    @else
+    <h2>Other users</h2>
+    @endif
     <a href="/">Welcom Page</a>
     <a href="/about/Raushan">About Page</a>
     <a href="/contact">Contact me</a>
