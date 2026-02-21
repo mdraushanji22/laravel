aecf2df5cc3ae64d31da58fd38745f0fd757768a
mdraushan <mdraushanji22@gmail.com>
2026-02-21 23:49:33 +0530

    PHP-Laravel

--- learn-laravel/resources/views/user-form.blade.php
+++ learn-laravel/resources/views/user-form.blade.php
@@ -1,4 +1,54 @@
 <div>
     <h2>Add New User</h2>
-    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
+
+    <form action="" method="post">
+        <div class="user-form">
+            <input type="text" placeholder="Enter your name" name="username" require>
+        </div>
+        <div class="user-form">
+            <input type="email" placeholder="Enter your email" name="email" require>
+        </div>
+        <div class="user-form">
+            <input type="text" placeholder="Enter your city" name="city" require>
+        </div>
+        <div class="user-form">
+            <button>Add User</button>
+        </div>
+    </form>
 </div>
+<style>
+    input {
+        width: 40%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+        /* Ensures padding/border are included in width */
+        background-color: #f8f8f8;
+        font-size: 16px;
+    }
+
+    button {
+        width: 40%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        background-color: #4CAF50;
+        color: white;
+        font-size: 16px;
+        cursor: pointer;
+        box-sizing: border-box;
+        transition: background-color 0.3s ease;
+    }
+
+    button:hover {
+        background-color: #45a049;
+    }
+
+    .user-form {
+        margin: 10px;
+    }
+</style>
